4a3354efddd171e8144d703ffa790ba293b44f9c
Mathias Zunino <mathiaszunino@gmail.com>
2021-04-07 04:59:42 -0300

    feat: navbar component

---
+++ src/components/Navbar/Menu.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-menu transition="slide-y-transition">
+    <template v-slot:activator="{ on }">
+      <v-btn fab icon v-on="on" class="hidden-sm-and-up">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </template>
+    <v-list>
+      <v-list-item>
+        <v-btn text tile color="primary" block to="/">Home</v-btn>
+      </v-list-item>
+      <v-list-item>
+        <v-btn text tile color="primary" block to="/about">About</v-btn>
+      </v-list-item>
+      <v-list-item>
+        <v-btn text tile color="primary" block to="/contact">Contact</v-btn>
+      </v-list-item>
+      <v-list-item>
+        <a href="/files/Resume.pdf" download target="_blank">
+          <v-btn color="primary" dark class="mx-2">Resume</v-btn>
+        </a>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({})
+export default class Menu extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+}
+</style>
---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="nav">
+    <router-link to="/">
+      <img src="@/assets/images/logo.png" alt="Logo" class="nav__logo" />
+    </router-link>
+    <Menu />
+    <div class="nav__items hidden-xs-only">
+      <v-btn class="pa-5" text tile color="primary" to="/">Home</v-btn>
+      <v-btn class="pa-5" text tile color="primary" to="/about">About</v-btn>
+      <v-btn class="pa-5" text tile color="primary" to="/contact">Contact</v-btn>
+      <a href="/files/Resume.pdf" download target="_blank">
+        <v-btn color="primary" dark class="mx-2">Resume</v-btn>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import Menu from "@/components/Navbar/Menu.vue";
+
+@Component({
+  components: {
+    Menu
+  }
+})
+export default class Navbar extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  padding: 1.4rem 0.5rem 0px;
+  margin: 0 5% !important;
+  display: flex;
+  justify-content: space-between;
+
+  &__logo {
+    width: 78px;
+  }
+
+  &__items {
+    align-self: center;
+  }
+}
+
+a {
+  text-decoration: none;
+}
+</style>
