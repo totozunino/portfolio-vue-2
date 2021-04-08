30d4304d217ff1e674dd01f2a2860f3c59b13ab4
Mathias Zunino <mathiaszunino@gmail.com>
2021-04-07 23:08:29 -0300

    feat: about view

---
+++ src/components/Project.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-card class="project" width="325" min-height="290">
+    <v-chip class="ma-2 project__chip" :color="project.status == 'Online' ? 'primary' : '#ff9b54'" x-small dark>
+      {{ project.status }}
+    </v-chip>
+    <v-card-title>
+      <v-img :src="project.logo" alt="Logo" max-width="74" class="mt-2"></v-img>
+      <v-btn icon x-large class="ml-auto mb-auto" @click="openProject(project.source)">
+        <v-icon color="#2C3E50">mdi-github</v-icon>
+      </v-btn>
+    </v-card-title>
+    <v-card-text class="px-8 pb-0">
+      <p class="project__title">{{ project.name }}</p>
+      <p class="project__info">
+        {{ project.description }}
+      </p>
+    </v-card-text>
+    <v-card-actions class="pt-0">
+      <v-chip small v-for="tech in project.technologies" :key="tech.name" color="#ECF1EB" class="mx-1">
+        <v-img :src="require(`@/assets/images/${tech.src}`)" max-width="16" max-height="16" class="mr-1"></v-img>
+        {{ tech.name }}
+      </v-chip>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  }
+})
+export default class Project extends Vue {
+  private openProject(link: string) {
+    window.open(link, "_blank");
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.project {
+  transition: all 0.3s ease;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  &:hover {
+    transform: translateY(-15px);
+  }
+
+  &__title {
+    font-size: 1.5rem;
+    font-weight: 100;
+    letter-spacing: 1px;
+    color: #00343d;
+  }
+
+  &__info {
+    font-size: 0.8rem;
+    font-weight: 100;
+    letter-spacing: 1px;
+    color: #00343d;
+  }
+
+  &__chip {
+    position: absolute;
+    top: -3px;
+    left: -23px;
+    transform: rotate(-20deg);
+  }
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,109 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-row no-gutters justify="center">
+      <v-col cols="12" sm="10" md="8">
+        <p class="about__description text-center">Some Things I've Built</p>
+        <v-row no-gutters>
+          <v-col v-for="project in projects" :key="project.name" class="pa-4" cols="12" sm="12" md="6" lg="4">
+            <project :project="project" class="mx-auto" />
+          </v-col>
+        </v-row>
+        <p class="about__description text-center">Where I've Worked</p>
+        <Tabs class="mx-6" />
+      </v-col>
+    </v-row>
   </div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import Project from "@/components/Project.vue";
+import Tabs from "@/components/Tabs/Tabs.vue";
+
+@Component({
+  components: {
+    Project,
+    Tabs
+  }
+})
+export default class About extends Vue {
+  private projects = [
+    {
+      name: "Portfolio",
+      description: "A single page web app for helping me organize my tasks, built with Nuxt.js, Firestore, and Vuetify",
+      logo: require("../assets/images/logo.png"),
+      source: "https://gitlab.com/totoz/day-tasks",
+      technologies: [
+        {
+          name: "Vue.js",
+          src: "vue-logo.png"
+        },
+        {
+          name: "Vuetify",
+          src: "vuetify.svg"
+        },
+        {
+          name: "Typescript",
+          src: "typescript.png"
+        }
+      ],
+      status: "Online"
+    },
+    {
+      name: "Avatar Maker",
+      description: "A single page web app for the fing, built in Nuxt.js, Firebase, and Vuetify",
+      logo: require("../assets/images/avatar.png"),
+      source: "",
+      technologies: [
+        {
+          name: "Vue.js",
+          src: "vue-logo.png"
+        },
+        {
+          name: "Vuetify",
+          src: "vuetify.svg"
+        },
+        {
+          name: "Typescript",
+          src: "typescript.png"
+        }
+      ],
+      status: "Dev"
+    },
+    {
+      name: "Tasku",
+      description: "A single page web app for helping me organize my projects, built in React",
+      logo: require("../assets/images/tasku-logo.jpg"),
+      source: "https://gitlab.com/totoz/engage-projects",
+      technologies: [
+        {
+          name: "Vue.js",
+          src: "vue-logo.png"
+        },
+        {
+          name: "Vuetify",
+          src: "vuetify.svg"
+        },
+        {
+          name: "Typescript",
+          src: "typescript.png"
+        }
+      ],
+      status: "Dev"
+    }
+  ];
+}
+</script>
+
+<style lang="scss" scoped>
+.about {
+  margin-bottom: 6rem;
+  &__description {
+    font-size: 1.8rem;
+    font-weight: 100;
+    letter-spacing: 1.5px;
+    color: #00343d;
+    margin: 4rem 0;
+  }
+}
+</style>
